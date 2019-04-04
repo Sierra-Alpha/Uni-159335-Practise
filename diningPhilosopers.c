de1a57389193fe19b1eeab6c35bd9a1ad73ec50c
Shaun Alexander <shaun@sierraalpha.co.nz>
2019-04-04 14:36:32 +1300

    first attempt

---
+++ diningPhilosopers.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define NUM_THREADS 5
+pthread_mutex_t lock[5];
+
+// The function executed by each thread
+void *Philosopher(void *threadid) {
+	int tid;
+	tid = (int)threadid;
+
+    int left = tid;
+    int right = (tid + 1) % 5;
+    while(true) {
+        int randThink = rand() % 20;
+        int randEat = rand() % 20;
+        printf("I'm philosopher %d and I'm thinking for %d\n", tid, randThink);
+        sleep(randThink);
+
+        switch (tid % 2) {
+
+            case 0:
+                pthread_mutex_lock(&lock[left]);
+                printf("I'm philosopher %d and I picked up my left spoon\n", tid);
+                pthread_mutex_lock(&lock[right]);
+                printf("I'm philosopher %d and I picked up my right spoon\n", tid);
+                break;
+
+            default:
+                pthread_mutex_lock(&lock[right]);
+                printf("I'm philosopher %d and I picked up my right spoon\n", tid);
+                pthread_mutex_lock(&lock[left]);
+                printf("I'm philosopher %d and I picked up my left spoon\n", tid);
+        }
+
+        // Eat using the spoons
+        printf("I'm philosopher %d and I'm eating for %d\n", tid, randEat);
+        sleep(randEat);
+
+        switch (tid % 2) {
+
+            case 0:
+                pthread_mutex_unlock(&lock[left]);
+                printf("I'm philosopher %d and I put down my left spoon\n", tid);
+                pthread_mutex_unlock(&lock[right]);
+                printf("I'm philosopher %d and I put down my right spoon\n", tid);
+                break;
+
+            default:
+                pthread_mutex_unlock(&lock[right]);
+                printf("I'm philosopher %d and I put down my right spoon\n", tid);
+                pthread_mutex_unlock(&lock[left]);
+                printf("I'm philosopher %d and I put down my left spoon\n", tid);
+        }
+    }
+	// Terminate thread
+	pthread_exit(NULL);
+}
+
+// Main Function
+int main (int argc, char *argv[]) {
+    srand(time(NULL));
+	// Will create 5 threads, so will need a reference to each
+	pthread_t threads[NUM_THREADS];
+
+	long n;
+	for(n = 0; n < NUM_THREADS; ++n) {
+		// Print
+		printf("In main: creating thread Philosopher %ld\n", n);
+
+		// Create a thread here, and get the thread to run the PrintHello
+		// function. Note the way the parameters are passed
+		int rc = pthread_create(&threads[n], NULL, Philosopher, (void *)n);
+
+		// A non-zero return code means something went wrong
+		if (rc) {
+			printf("ERROR; return code from pthread_create() is %d\n", rc);
+			exit(-1);
+		}
+	}
+
+	// Useful to call this in the main to ensure that all threads are kept alive.
+	// Otherwise threads that are still going will be automatically terminated
+	pthread_exit(NULL);
+}
+
+
+
