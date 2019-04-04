e0c2b7d86e3eea96f5bf33276cc7512fdf04a859
Shaun Alexander <shaun@sierraalpha.co.nz>
2019-04-04 15:13:50 +1300

    removed spoon grab comments

--- diningPhilosopers.c
+++ diningPhilosopers.c
@@ -19,41 +19,43 @@ void *Philosopher(void *threadid) {
         int randEat = rand() % 20;
         printf("I'm philosopher %d and I'm thinking for %d\n", tid, randThink);
         sleep(randThink);
+        printf("I'm philosopher %d and I'm finished thinking, waiting to eat\n", tid);
 
         switch (tid % 2) {
 
             case 0:
                 pthread_mutex_lock(&lock[left]);
-                printf("I'm philosopher %d and I picked up my left spoon\n", tid);
+                // printf("I'm philosopher %d and I picked up my left spoon\n", tid);
                 pthread_mutex_lock(&lock[right]);
-                printf("I'm philosopher %d and I picked up my right spoon\n", tid);
+                // printf("I'm philosopher %d and I picked up my right spoon\n", tid);
                 break;
 
             default:
                 pthread_mutex_lock(&lock[right]);
-                printf("I'm philosopher %d and I picked up my right spoon\n", tid);
+                // printf("I'm philosopher %d and I picked up my right spoon\n", tid);
                 pthread_mutex_lock(&lock[left]);
-                printf("I'm philosopher %d and I picked up my left spoon\n", tid);
+                // printf("I'm philosopher %d and I picked up my left spoon\n", tid);
         }
 
         // Eat using the spoons
         printf("I'm philosopher %d and I'm eating for %d\n", tid, randEat);
         sleep(randEat);
+        printf("I'm philosopher %d and I finished eating\n", tid);
 
         switch (tid % 2) {
 
             case 0:
                 pthread_mutex_unlock(&lock[left]);
-                printf("I'm philosopher %d and I put down my left spoon\n", tid);
+                // printf("I'm philosopher %d and I put down my left spoon\n", tid);
                 pthread_mutex_unlock(&lock[right]);
-                printf("I'm philosopher %d and I put down my right spoon\n", tid);
+                // printf("I'm philosopher %d and I put down my right spoon\n", tid);
                 break;
 
             default:
                 pthread_mutex_unlock(&lock[right]);
-                printf("I'm philosopher %d and I put down my right spoon\n", tid);
+                // printf("I'm philosopher %d and I put down my right spoon\n", tid);
                 pthread_mutex_unlock(&lock[left]);
-                printf("I'm philosopher %d and I put down my left spoon\n", tid);
+                // printf("I'm philosopher %d and I put down my left spoon\n", tid);
         }
     }
 	// Terminate thread
@@ -66,9 +68,14 @@ int main (int argc, char *argv[]) {
 	// Will create 5 threads, so will need a reference to each
 	pthread_t threads[NUM_THREADS];
 
+
 	long n;
 	for(n = 0; n < NUM_THREADS; ++n) {
 		// Print
+    if (pthread_mutex_init(&lock[n], NULL) != 0) {
+        printf("\n mutex init has failed\n");
+        return 1;
+    }
 		printf("In main: creating thread Philosopher %ld\n", n);
 
 		// Create a thread here, and get the thread to run the PrintHello
